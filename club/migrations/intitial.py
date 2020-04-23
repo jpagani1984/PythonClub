2f1d86cdff648dd9ed6add710b204874567c4ae3
jpagani1984 <38275742+jpagani1984@users.noreply.github.com>
2020-04-23 10:35:39 -0700

    Add files via upload

---
+++ club/migrations/intitial.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Club',
+            fields=[
+               
+            ],
+        ),
+    ]
+    
