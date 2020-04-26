4da7e557b9d8f5b038104714ca4c9ca84e7e1923
jpagani1984 <38275742+jpagani1984@users.noreply.github.com>
2020-04-26 09:16:26 -0700

    Add files via upload

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Meeting
+from .models import MeetingMinutes
+from .models import Resource
+from .models import Event
 
-# Register your models here.
+admin.site.register(Meeting)
+admin.site.unregister(MeetingMinutes)
+admin.site.unregister(Resource)
+admin.site.unregister(Event)
+
+admin.site.site_header = "Python Club"
+
+
+
--- club/models.py
+++ club/models.py
@@ -1,3 +1,67 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime
+import re
 
-# Create your models here.
+class MeetingManager(models.Manager):
+    def createMeeting(self, postData, user_id):
+        response = {
+            'status' : False,
+            'errors' : []
+        }
+        if len(postData['title']) < 2:
+            response['errors'].append('invalid item FRIEND')
+        if len(response['errors']) == 0:
+            response['status'] = True
+            user_id = Users.objects.get(id = user_id)
+            title = Title.objects.create(
+                title = postData['title'],
+                added_by = user_id
+            )
+            title.other.add(user_id)
+            title.save()
+        return response
+    def createOther(self, user_id, title_id):
+        user_id = Users.objects.get(id = user_id)
+        title = Title.objects.get(id = title_id)
+        title.other.add(user_id)
+        title.save()
+
+    def removeTitle(self, user_id, title_id):
+        user_id = Users.objects.get(id = user_id)
+        title = Title.objects.get(id = title_id)
+        title.other.remove(user_id)
+        title.save()
+    
+    def deleteTitle(self, user_id, title_id):
+        title = Title.objects.get(id = title_id)
+        title.delete()
+
+class Meeting(models.Model):
+    meeting_title = models.CharField(max_length = 255)
+    meeting_date = models.DateTimeField(auto_now_add = True)
+    meeting_time = models.DateTimeField(auto_now_add = True)
+    meeting_location = models.CharField(max_length = 255)
+    meeting_agenda = models.CharField(max_length = 255)
+
+class MeetingMinutes(models.Model)
+    meeting_id = models.models.ForeignKey(Users, related_name = 'added_by', null=True)
+    attendance = models.ManyToManyField(Users, related_name = 'other_user')
+    minutes = models.CharField(max_length = 255)
+
+class Resource(models.Model)
+    resource_type = models.CharField(max_length = 255)
+    URL = models.CharField(max_length = 255)
+    date_entered = models.DateTimeField(auto_now_add = True)
+    user_id = models.models.ForeignKey(Users, related_name = 'added_by', null=True)
+    description = models.CharField(max_length = 255)
+
+class Event(models.Model)
+    event_title = models.CharField(max_length = 255)
+    event_location = models.CharField(max_length = 255)
+    event_date = models.DateTimeField(auto_now_add = True)
+    event_time = models.DateTimeField(auto_now_add = True)
+    event_description = models.CharField(max_length = 255)
+    user_id = models.models.ForeignKey(Users, related_name = 'added_by', null=True)
+    
+        
