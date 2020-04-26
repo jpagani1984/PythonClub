9a28d5483b472f2285fb1f15b43d0bc87bc94e8a
jpagani1984 <38275742+jpagani1984@users.noreply.github.com>
2020-04-26 09:15:45 -0700

    Add files via upload

--- club/migrations/intitial.py
+++ club/migrations/intitial.py
@@ -11,10 +11,17 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Club',
+            name='Title',
             fields=[
-               
-            ],
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('meeting_id', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='added_by', to='users.Users')),
+                ('user_id', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='added_by', to='users.Users')),
+                ('attendance', models.ManyToManyField(related_name='other_user', to='users.Users')),
+                ('event_time', models.DateTimeField(auto_now_add=True)),
+                ('event_date', models.DateTimeField(auto_now_add=True)),
+                ('meeting_time', models.DateTimeField(auto_now_add=True)),
+                ('meeting_date', models.DateTimeField(auto_now_add=True)),
+                ],
         ),
     ]
     
