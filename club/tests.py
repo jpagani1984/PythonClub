5eff99bb8e0ed6cfdf2adb149918c7fd2a77a757
jpagani1984 <38275742+jpagani1984@users.noreply.github.com>
2020-05-31 09:46:00 -0700

    Add files via upload

--- club/tests.py
+++ club/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import Product, ProductType, Review
+from .views import index, gettypes, getproducts
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class MeetingTypeTest(TestCase):
+       def test_string(self):
+       type=MeetingType(typename="Tablet")
+       self.assertEqual(str(type), type.typename)
+
+   def test_table(self):
+       self.assertEqual(str(MeetingType._meta.db_table), 'meetingtype')
+
+class ReviewTest(TestCase):
+       def test_string(self):
+       rev=Review(reviewtitle="Best Review")
+       self.assertEqual(str(rev), rev.reviewtitle)
+
+   def test_table(self):
+       self.assertEqual(str(Review._meta.db_table), 'reviews')
+
+class IndexTest(TestCase):
+       def test_view_url_accessible_by_name(self):
+       response = self.client.get(reverse('index'))
+       self.assertEqual(response.status_code, 200)
+  
+class GetProductsTest(TestCase):
+   def test_view_url_accessible_by_name(self):
+       response = self.client.get(reverse('products'))
+       self.assertEqual(response.status_code, 200)
+def test_discount(self):
+        discount=self.prod.memberdiscount()
+        self.assertEqual(discount, 25.00)
+
+    def test_number_of_reviews(self):
+        reviews=Review.objects.filter(meeting=self.meeting).count()
+        self.assertEqual(reviews, 2)
