ea7a5c13a1259abcd544c975d9ebd38185eb00fc
jpagani1984 <38275742+jpagani1984@users.noreply.github.com>
2020-06-07 07:36:42 -0700

    Add files via upload

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('club/', include('club.urls'),
     path('index/', include('index.urls'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
